3910740c394b4ee1f016fe0e1970586cff13d471
nathan <ennemmjones@gmail.com>
2021-08-22 21:16:00 -0400

    commit to be graded

---
+++ Exercise 38/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Exercise_38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exitCondition = true;// set initial state of exit condition
+            while (exitCondition)
+            {
+
+                Console.WriteLine(AvgOfArray(5));
+
+                Console.WriteLine("Would you like to continue (y/n)?"); // prompt user to continue or exit
+
+                if (Console.ReadLine().ToLower() != "y") // user input to continue or exit
+                {
+                    exitCondition = false; // change condition to exit if user input other than y
+                    Console.WriteLine("Goodbye!");
+                }
+            }
+        }
+
+        public static string AvgOfArray(int num)
+        {
+            double[] numArray = new double[num];
+            double sum = 0;
+            int count = 0;
+            string stringSum = String.Empty;
+            while (count < num)
+            {
+                Console.Write("Enter a Number: ");
+                if (double.TryParse(Console.ReadLine(), out numArray[count]))   // if user inupt is valid add to array
+                {
+                    sum += numArray[count];  // add array element to sum
+                    stringSum += $"{numArray[count]} + "; // add to string listing all elements
+                    count++;
+                }
+
+                else
+                {
+                    Console.WriteLine("That was not a number, try again.");
+                }
+            }
+            
+            return $"({stringSum.Remove(stringSum.Length - 3)})/5 = {sum/5}";  // calculate average and output calculation as string
+
+        }
+    }
+}
